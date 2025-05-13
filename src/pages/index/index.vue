1e09c04347bc26054db93ea353b316a38295a56d
deeeeer12 <821552133@qq.com>
2025-05-13 16:43:48 +0800

    首页初始化完成

---
+++ src/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <view class="container">
+    <view class="menu-item" v-for="item in menuList" :key="item.id">
+      <text>{{ item.name }} - ¥{{ item.price }}</text>
+      <button @click="addToCart(item)">加入购物车</button>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuList: []
+    };
+  },
+  methods: {
+    addToCart(item) {
+      uni.$emit('addToCart', item);
+      uni.showToast({ title: '已加入购物车' });
+    }
+  },
+  onLoad() {
+    // 模拟请求菜单
+    this.menuList = [
+      { id: 1, name: '红烧牛肉面', price: 18 },
+      { id: 2, name: '鸡腿饭', price: 22 },
+      { id: 3, name: '酸辣土豆丝', price: 10 }
+    ];
+  }
+};
+</script>
+
+<style>
+.container {
+  padding: 20rpx;
+}
+.menu-item {
+  margin-bottom: 20rpx;
+}
+</style>
